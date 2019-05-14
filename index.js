735098c3f58dc7b0926f8f5db84073c62c6806e6
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 16:09:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ let findAdjacent = (vertex, vertices, edges) => {
     }
   })
   edgesWithVertex.map(edge => {
-    edge.filter(edgeVertix => {
+    edge.find(edgeVertix => {
       return edgeVertix != vertex
     })
   })
