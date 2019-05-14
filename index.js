69182f255cb88e094ab692846c59ee9a860871e0
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 16:44:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,24 +4,27 @@ function bfs(startNode, vertices, edges){
 
 let findVertex = (vertexName, vertices) => {
   return vertices.find(vertex => {
-    return vertex.name == vertexName
-  })
+    return vertex.name == vertexName;
+  });
 }
 
 let findAdjacent = (vertex, vertices, edges) => {
   return edges.filter(edge => {
-    return edge.includes(vertex)
+    return edge.includes(vertex);
   }).map(edge => {
     return edge.filter(node => {
-      return node != vertex
-    })
+      return node != vertex;
+    });
   }).map(vertexName => {
-    return findVertex(vertexName, vertices)
+    return findVertex(vertexName, vertices);
   }).filter(node => {
-    return node.distance == null
-  })
+    return node.distance == null;
+  });
 }
 
 let markDistanceAndPredecessor = (vertex, adjacentVertices)=> {
-
+  adjacentVertices.map(adjacentVertex => {
+    adjacentVertex.distance = vertex.distance + 1;
+    adjacentVertex.predecessor = vertex;
+  });
 }
