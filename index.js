35e007f18c655f8262b4d1e2400e756ee86d7808
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 16:14:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ function bfs(startNode, vertices, edges){
 let findAdjacent = (vertex, vertices, edges) => {
   console.log(edges.filter(edge => {
     edge.includes(vertex)
-  }).map(edges.filter(node => {
+  }).map(edge => {
+    edge.filter(node => {
     node != vertex
   }))
 )
