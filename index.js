42e2dbe5fcfed6e83f2bd29f6a38f3da25c86c16
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 15:49:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
-function bfs(rootNode, vertices, edges){
+function bfs(startNode, vertices, edges){
+
+}
+
+let findAdjacent = (vertex, vertices, edges) => {
+  let adjacentVertices = [];
+  let edgesWithVertex = [];
+  edges.forEach(edge => {
+    if (edge.includes(vertex)) {
+      edgesWithVertex.push(edge)
+    }
+  })
+  edgesWithVertex.forEach(edge => {
+    edge.forEach(edgeVertix => {
+      edgeVertix != vertex ? adjacentVertices.push(edgeVertix) : ''})
+  })
+  adjacentVertices = vertices.map(vertex => {
+    vertex.name == adjacentVertices[0] || vertex.name == adjacentVertices[1]
+  })
+  return adjacentVertices
+}
+
+
+let markDistanceAndPredecessor = (vertex, adjacentVertices)=> {
 
 }
