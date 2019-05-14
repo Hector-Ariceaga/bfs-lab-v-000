89fa5bc874d4b4f207dcc1ab20ee844066156308
HECTOR ARICEAGA <hector_ariceaga@hotmail.com>
2019-05-14 16:13:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,18 +9,6 @@ let findAdjacent = (vertex, vertices, edges) => {
     node != vertex
   }))
 )
-  edgesWithVertex.map(edge => {
-    edge.find(edgeVertix => {
-      return edgeVertix != vertex
-    })
-  })
-  // adjacentVertices = vertices.filter(vertex => {
-  //     if (vertex == edgesWithVertex[0] || vertex == edgesWithVertex [1]) {
-  //       return vertex
-  //     }
-  //   })
-    console.log(edgesWithVertex)
-  return adjacentVertices
 }
 
 
